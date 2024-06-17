4a1d9480bd29b6c79abad936cafd788460caa4c6
Omkar Nalawade <105204391+Omkarrrrrrr@users.noreply.github.com>
2024-06-17 08:15:36 +0530

    Add files via upload

---
+++ Object_creation.java
@@ -0,0 +1,13 @@
+class Object_creation
+{
+    int a;
+    String name;
+
+    public static void main(String[] args){
+        // Declare the variable
+        // create the object
+
+        int num = 9;
+        Test obj = new Test();
+    }
+}
