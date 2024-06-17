f69ea454f082fa6be7d30dd511785c544baf3eb2
Omkar Nalawade <105204391+Omkarrrrrrr@users.noreply.github.com>
2024-06-17 08:51:18 +0530

    Add files via upload

---
+++ Object_creation2.java
@@ -0,0 +1,23 @@
+class Object_creation2
+{
+    int a = 2;
+    String name = "Rahul";
+
+    public static void main(String[] args){
+        // Declare the variable
+        // create the object
+
+        int num = 9;
+        Test obj1 = new Test();
+        Test obj2 = new Test();
+        
+        
+        obj1.name = "Navin";
+        
+        System.out.println(obj1.a);
+        System.out.println(obj1.name);
+
+        System.out.println(obj2.a);
+        System.out.println(obj2.name);
+    }
+}
